02d4eb7dee94e1244b97d05fb894bc36865eec9f
Sianats <a.torres.2018@alumnos.urjc.es>
2021-03-18 10:45:56 +0100

    ejemplo3 que no entiendo para que sirve

---
+++ S5/ejemplo3/contadorobjetos.js
@@ -0,0 +1,16 @@
+const gui = {
+  display: document.getElementById("display"),
+  boton: document.getElementById("boton"),
+}
+
+const counter = {
+  valor: 0,
+  inc : function(value) {
+    this.valor += value;
+    gui.display.innerHTML = this.valor;
+  }
+}
+
+gui.boton.onclick = () => {
+  counter.inc(1)
+}
