0ebed28872ac2ceceda34efe063aab7e36a34d09
Sianats <a.torres.2018@alumnos.urjc.es>
2021-03-18 12:16:38 +0100

    calculadora

--- S5/ejemplo8/calculadora.js
+++ S5/ejemplo8/calculadora.js
@@ -1,11 +1,14 @@
 const botones = document.getElementsByClassName("digito");
 
+
+
 function digito(value)
 {
   console.log("Valor: " + value);
 }
 
 for (let boton of botones) {
+
 
   boton.onclick = (ev) => {
     digito(ev.target.value)
