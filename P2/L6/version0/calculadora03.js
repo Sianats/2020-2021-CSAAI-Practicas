1b3ef30c5ce812e1966e3147279a8d455b48c14c
Sianats <a.torres.2018@alumnos.urjc.es>
2021-03-23 11:55:35 +0100

    calculadora 0.3

---
+++ P2/L6/version0/calculadora03.js
@@ -0,0 +1,28 @@
+display = document.getElementById("display")
+boton1 = document.getElementsByClassName("boton1")
+boton2 = document.getElementsByClassName("boton2")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+
+boton1.onclick = (ev) => {
+    display.innerHTML += ev.target.value;
+}
+  
+boton2.onclick = (ev) => {
+    display.innerHTML += ev.target.value;
+}
+  
+suma.onclick = (ev) => {
+    display.innerHTML += ev.target.value;
+}
+  
+  igual.onclick = (ev) => {
+    display.innerHTML = eval(display.innerHTML);
+}
+  
+  clear.onclick = (ev) => {
+    display.innerHTML = "0";
+}
+  
