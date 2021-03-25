b2178a49b990d2d8007d1671587210291427afcc
Sianats <a.torres.2018@alumnos.urjc.es>
2021-03-25 14:15:09 +0100

    pruebas

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -1,13 +1,46 @@
-const botones = document.getElementsByClassName("digito");
+display = document.getElementById("display")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
 
-function digito(value)
-{
-  console.log("Valor: " + value);
+const ESTADO = {
+  INIT: 0,
+  OP1: 1,
+  OPERATION: 2,
+  OP2: 4,
 }
 
-for (let boton of botones) {
+let estado = ESTADO.INIT; 
 
-  boton.onclick = (ev) => {
-    digito(ev.target.value)
-  }
+function digito(ev){
+  if (estado == ESTADO.INIT) {
+    display.innerHTML = ev.target.value;
+    estado = ESTADO.OP1;
+  } else {
+    display.innerHTML += ev.target.value;
+  }    
+}
+
+
+digitos = document.getElementsByClassName("digito")
+
+for (let boton of digitos) {
+  boton.onclick = digito;
+}
+
+suma.onclick = (ev) => {
+  display.innerHTML += ev.target.value;
+}
+
+resta.onclick = (ev) => {
+  display.innerHTML -= ev.target.value;
+}
+
+igual.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+}
+
+clear.onclick = () => {
+  display.innerHTML = "0";
+  estado = ESTADO.INIT;
 }
