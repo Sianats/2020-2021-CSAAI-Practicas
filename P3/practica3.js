b824af9ac5cb6457eb89d9942d6bf85f2321faa2
Sianats <a.torres.2018@alumnos.urjc.es>
2021-04-28 19:41:28 +0200

    cambios

--- P3/practica3.js
+++ P3/practica3.js
@@ -1,13 +1,32 @@
 console.log("Ejecutando JS...");
 
-reset = document.getElementById("reset")
+// Declarar el canvas
 const canvas = document.getElementById("canvas");
-const radio = 7;
+canvas.width = 350;
+canvas.height = 500;
+const ctx = canvas.getContext("2d");
+canvas.addEventListener( 'click', update, false );
+
+// Declarar ciertas cosas
 let puntuacion;
 let vidas;
 let raqueta;
+
+// Coordenadas iniciales de la bola
+let bolax;
+let bolay;
+
+// Velocidades y radio de la bola
+let velx = 3;
+let vely = 3;
+const radio = 7;
+
+// Caracteristicas raqueta
 const anchoraqueta= 10;
 const largoraqueta= 75;
+raqueta= (canvas.width - largoraqueta) / 2;
+
+// Posicion de los ladrillos
 const origen_x= 0;
 const origen_y= 45;
 const LADRILLO = {
@@ -19,12 +38,6 @@ const LADRILLO = {
   visible: true
 };
 
-
-raqueta= (canvas.width - largoraqueta) / 2;
-var derecha;
-var izquierda;
-
-
 //function moverConRaton(e) {
 //    var localiz = e.clientX - canvas.offsetLeft;
 //   if ( localiz > 0 && localiz < canvas.width ) {
@@ -32,65 +45,33 @@ var izquierda;
 //   }
 //}
 
-
 function drawRaqueta() {
     ctx.beginPath();
     ctx.rect(raqueta, canvas.height - anchoraqueta, largoraqueta, anchoraqueta);
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "blue";
     ctx.fill();
     ctx.closePath();
 }
-//-- Definir el tamaño del canvas
-canvas.width = 350;
-canvas.height = 500;
 
-//-- Obtener el contexto del canvas
-const ctx = canvas.getContext("2d");
-
-//-- Coordenadas del objeto
-let x = 10;
-let y =10;
-
-//-- Velocidades del objeto
-let velx = 1;
-let vely = 1;
-
-
+function drawbola (){
+  ctx.beginPath();
+  //-- Dibujar
+  ctx.arc( bolax, bolay, radio, 0, Math.PI * 2 );
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  ctx.closePath();
+}
 //document.addEventListener( "mousemove", moverConRaton, false );
-canvas.addEventListener( 'click', update, false );
+
 //-- Funcion principal de animacion
 function update() 
 {
+
   vidas= 3;
   puntuacion= 0;
-  //-- Algoritmo de animacion:
-  //-- 1) Actualizar posicion del  elemento
-  //-- (física del movimiento rectilineo uniforme)
-
-   //-- Condicion de rebote en extremos verticales del canvas
-   if (x < 7 || x >= canvas.width - radio) {
-    velx = -velx;
-  }
-
-  //-- Condición de rebote en extremos horizontales del canvas
-  if (y <= radio || y > canvas.height - radio) {
-    vely = -vely;
-  }
-
-  //-- Actualizar la posición
-  x = x + velx;
-  y = y + vely;
-
   //-- 2) Borrar el canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  //-- 3) Dibujar los elementos visibles
-  ctx.beginPath();
-
-  //-- Dibujar
-  ctx.arc( x, y, radio, 0, Math.PI * 2 );
-  ctx.fillStyle = 'white';
-
+ ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
 //-- Estructura de los ladrillos
 const ladrillos = [];
 
@@ -112,17 +93,17 @@ for (let i = 0; i < LADRILLO.F; i++) {
 //-- Dibujar ladrillos
 for (let i = 0; i < LADRILLO.F; i++) {
     for (let j = 0; j < LADRILLO.C; j++) {
-
       //-- Si el ladrillo es visible se pinta
       if (ladrillos[i][j].visible) {
         ctx.beginPath();
         ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
         ctx.fillStyle = 'white';
+        }
         ctx.fill();
         ctx.closePath();
       }
     }
-}
+
   //-- Rellenar
   ctx.fill();
 
@@ -131,12 +112,76 @@ for (let i = 0; i < LADRILLO.F; i++) {
   ctx.closePath();
 
 
-  //-- 4) Volver a ejecutar update cuando toque
-  requestAnimationFrame(update);
+
   drawScore();
   drawLives();
   drawRaqueta();
+  drawbola();
+  bola();
+    //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
 
+function bola(){ 
+   bolax = canvas.width / 2;
+  bolay = canvas.height - 50;
+   //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+
+   //-- Condicion de rebote en extremos verticales del canvas
+   if (bolax < 7 || bolax >= canvas.width - radio) {
+    velx = -velx;
+  }
+
+  //-- Condición de rebote en extremos horizontales del canvas
+  if (bolay <= radio || bolay > canvas.height - radio) {
+    vely = -vely;
+  }
+
+
+
+  //-- Actualizar la posición
+  bolax = bolax + velx;
+  bolay = bolay + vely; 
+}
+
+function restart (ev){
+  if (ev.keyCode =='32'){
+    update();
+  }
+}
+
+function moverraqueta(ev){
+    if (ev.keyCode =='39'){
+      ctx.filltext("derecha");
+  } else if (ev.keyCode =='37'){
+    ctx.filltext("izquierda");
+  }
+}
+
+//todavia no sé si funciona porque no se me mueve la bola ya
+function romperladrillos() {
+  
+  for ( var i = 0; i < LADRILLO.F; i++ ) {
+      for ( var j = 0; j < LADRILLO.C; j++ ) {
+      var b = LADRILLO[i][j];
+      if ( b.status == 1 ) {
+          if ( bolax > b.x && bolax < b.x + LADRILLO.w && bolay > b.y && bolay < b.y + LADRILLO.h ) {
+          vely = -vely;
+          b.status = 0;
+          puntuacion++;
+          if ( puntuacion == LADRILLO.C * LADRILLO.F ) {
+              ctx.textAlign = "center";
+              ctx.font = "24px CharriotDeluxe";
+              ctx.fillStyle = '#fff';
+              ctx.fillText( "¡Ganaste!", 240, 140 );
+              document.addEventListener( 'restart', update, false );
+          }
+          }
+      }
+      }
+  }
 }
 
 function drawScore() {
@@ -151,7 +196,8 @@ function drawLives() {
   ctx.font = "40px CharriotDeluxe";
   ctx.fillStyle = "white";
   ctx.fillText( "00" + vidas, canvas.width - 70, 30 );
-}
+} 
+
 //-- ¡Que empiece la función!
 update();
 
