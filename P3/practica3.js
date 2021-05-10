638a3ffcfcb03f89c2871e0f3200e322bf49813f
Sianats <a.torres.2018@alumnos.urjc.es>
2021-05-10 13:52:04 +0200

    cambios finales + comentarios al codigo

--- P3/practica3.js
+++ P3/practica3.js
@@ -12,9 +12,8 @@ const alturaladrillo = 10;
 const Paddingladrillo = 5;
 const bricktop = 50;
 const brickleft = 0;
-const anchoraqueta = 95;
 const altoraqueta = 10;
-const radio = 10;
+const radio = 8;
 
 // Efectos de sonido
 const golperaqueta = new Audio('golperaqueta.wav');
@@ -23,7 +22,6 @@ const golpeladrillo = new Audio('golpeladrillo.wav');
 
 // Estilo de la letra
 ctx.font = "50px CharriotDeluxe";
-ctx.fillStyle = "white";
 ctx.textAlign = "left";
 
 // Declarar ciertas cosas
@@ -34,6 +32,7 @@ let raqueta;
 let jugar;
 let izquierda;
 let derecha;
+let anchoraqueta = 95;
 
 // Coordenadas iniciales de la bola
 let x;
@@ -43,12 +42,14 @@ let y;
 var velx;
 var vely;
 
+// Se usa la funcion restart al hacer click en canvas
 canvas.addEventListener( 'click', restart, false );
 
 function update() {
+    // No podemos restablecer el juego mientras jugamos
     canvas.removeEventListener( 'click', restart, false );
 
-    // array para los ladrillos
+    // Array para los ladrillos
     LADRILLO = [];
     for ( var i = 0; i < filas; i++ ) {
         LADRILLO[i] = [];
@@ -59,6 +60,7 @@ function update() {
 
     // valores iniciales
     raqueta = ( canvas.width - anchoraqueta ) / 2;
+    anchoraqueta = 95;
     x = canvas.width / 2;
     y = canvas.height - 40;
     jugar = false;
@@ -67,9 +69,12 @@ function update() {
     izquierda = false;
     derecha = false;
 
-    // Funcion que voy a crear para que dibuje las cosas
+    // Funcion dibujar los elementos
     draw();
 }
+
+// Funciones de modo, dependiendo de qué boton pulses,
+// la bola se mueve a una velocidad distinta.
 
 facil.onclick = () =>{
     velx = 3;
@@ -81,8 +86,8 @@ facil.onclick = () =>{
 }
 
 medio.onclick = () =>{
-    velx = 5;
-    vely = -5;
+    velx = 4;
+    vely = -4;
     facil.onclick = false;
     medio.onclick = false;
     dificil.onclick = false;
@@ -90,28 +95,29 @@ medio.onclick = () =>{
 }
 
 dificil.onclick = () =>{
-    velx = 7;
-    vely = -7;
+    velx = 6;
+    vely = -6;
     facil.onclick = false;
     medio.onclick = false;
     dificil.onclick = false;
 
 }
 
+// Dependiendo de que tecla pulsemos, tiene una funcion u otra
 document.onkeydown = (e)=> {
     switch (e.keyCode) {
         case 37:
             izquierda = true;
 
             if ( raqueta > 0 ){
-                raqueta -= 7;
+                raqueta -= 9;
             }
         break;
 
         case 39:
             derecha = true;
             if ( raqueta < canvas.width - anchoraqueta ){
-                raqueta += 7;
+                raqueta += 9;
             }
         break;
 
@@ -121,6 +127,7 @@ document.onkeydown = (e)=> {
     }
 }
 
+// Funcion dibujar bola
 function drawbola() {
     ctx.beginPath();
     ctx.arc( x, y, radio, 0, Math.PI * 2 );
@@ -129,6 +136,7 @@ function drawbola() {
     ctx.closePath();
 }
 
+// Array para que desaparezcan los ladrillos al tocarlos con la bola.
 function romperladrillos() {
     for ( var i = 0; i < filas; i++ ) {
         for ( var j = 0; j < columnas; j++ ) {
@@ -149,41 +157,50 @@ function romperladrillos() {
                     } else if (i == 6 || i == 7){
                         puntuacion += 1;
                     }
-                    // Si ganas
-                    if ( puntuacion == columnas * filas ) {
-                        jugar = false;
-                        ctx.fillText( 'You won!', 150, 350 );
-                        canvas.addEventListener( 'click', restart, false );
-                    }
+
                 }
             }
         }
     }
+
+    // Si ganas
+    if ( puntuacion == 448 ) {
+        jugar = false;
+        ctx.fillStyle = "white";
+        ctx.fillText( 'You won!', 150, 350 );
+        canvas.addEventListener( 'click', restart, false );
+    }
 }
 
+// Funcion dibujar raqueta
 function drawraqueta() {
     ctx.beginPath();
     ctx.rect( raqueta, canvas.height - 20, anchoraqueta, altoraqueta );
     ctx.fillStyle = "blue";
     ctx.fill();
     ctx.closePath();
+
+    // Si llegamos a esa puntuacion, la raqueta mengua
+    if (puntuacion >= 149 && puntuacion < 298){
+        anchoraqueta = 75;
+    } else if(puntuacion >= 298){
+        anchoraqueta = 55;
+    }
 }
 
+// Dibujar la puntuación
 function drawpuntuacion() {
     ctx.fillStyle = "white";
-    ctx.fillText( "00" + puntuacion, 10, 40 );
-
+    ctx.fillText( puntuacion, 10, 40 );
 }
 
-// Points are 1 point for yellow, 3 for the green, 7 for red, orange 5. To make the game more challenging,
-//  the speed of the game increases after 4 hits and again at 12 hits and to the highest speeds in
-//   orange and red rows. The hammer decreases by half the size when the red row is broken through. 
+// Dibujar las vidas
 function drawvidas() {
     ctx.fillStyle = "white";
     ctx.fillText( "00" + vidas, canvas.width - 90, 40 );
-
 }
 
+// Funcion dibujar ladrillos con array
 function drawladrillos() {
     for ( var i = 0; i < filas; i++ ) {
         for ( var j = 0; j < columnas; j++ ) {
@@ -211,6 +228,7 @@ function drawladrillos() {
     }
 }
 
+// Funcion dibujar todos los elementos
 function draw(){
     ctx.clearRect( 0, 0, canvas.width, canvas.height );
     drawbola();
@@ -220,7 +238,7 @@ function draw(){
     drawladrillos();
     romperladrillos();
 
-
+    // Si pulsas el espacio, que empiece el juego
     if (jugar){
         if ( x + velx > canvas.width - radio || x + velx < radio ) {
             golpelado.play();
@@ -230,7 +248,7 @@ function draw(){
         if ( y + vely < 50 ) {
             golpelado.play();
             vely = -vely;
-        } else if ( y + vely > canvas.height - (radio + altoraqueta) ) {
+        } else if ( y + vely > canvas.height - 26 ) {
             if ( x > raqueta && x < raqueta + anchoraqueta ) {
                 golperaqueta.play();
                 vely = -vely;
@@ -253,15 +271,17 @@ function draw(){
         x += velx;
         y += vely;
 
-    } else if ( !vidas ) {
+    } else if ( !vidas ) { // Si pierdes todas las vidas
         ctx.fillStyle = 'white';
         ctx.fillText( 'Game Over', 150, 350 );
         canvas.addEventListener( 'click', restart, false);
     }
 
+    // Iniciar la animación
     requestAnimationFrame(draw);
 }
 
+// Recargar la página
 function restart () {
     document.location.reload();
 }
